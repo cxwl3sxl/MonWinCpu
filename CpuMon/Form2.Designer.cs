674ed1493793fbfc4e550b616050713bcd4e3689
SXL\xl <xinglin.sc@outlook.com>
2022-09-26 10:26:07 +0800

    界面优化

--- CpuMon/Form2.Designer.cs
+++ CpuMon/Form2.Designer.cs
@@ -31,6 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
             // label1
@@ -77,5 +78,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
--- CpuMon/Form2.cs
+++ CpuMon/Form2.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Management;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,9 +9,11 @@ namespace CpuMon
     public partial class Form2 : Form
     {
         private readonly ProcessInfo _processInfo;
-        private PerformanceCounter _cpuTimeCounter;
+        private ManagementObjectSearcher _mos;
         private int _x, _y;
-        private CounterSample _preSample = default;
+        DateTime _firstSample = DateTime.MinValue, _secondSample = DateTime.MinValue;
+        ulong _uOldCpu = 0;
+        private double _msPassed;
 
         public Form2(ProcessInfo processInfo, bool topMost)
         {
@@ -21,9 +24,12 @@ namespace CpuMon
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            toolTip1.SetToolTip(label1, $"正在监控{_processInfo.ProcessName}的CPU使用率，双击退出");
+
             Task.Factory.StartNew(() =>
             {
-                _cpuTimeCounter = new PerformanceCounter("Process", "% Processor Time", _processInfo.ProcessName);
+                var objQuery = new ObjectQuery("select * from Win32_Process WHERE ProcessID = " + _processInfo.Id);
+                _mos = new ManagementObjectSearcher(objQuery);
                 Invoke(new Action(timer1.Start));
             });
             Width = Height = 50;
@@ -31,9 +37,37 @@ namespace CpuMon
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            var sample1 = _cpuTimeCounter.NextSample();
-            label1.Text = $"{CounterSample.Calculate(_preSample, sample1)}";
-            _preSample = sample1;
+            var gets = _mos.Get();
+            foreach (ManagementObject mObj in gets)
+            {
+                try
+                {
+                    if (_firstSample == DateTime.MinValue)
+                    {
+                        _firstSample = DateTime.Now;
+                        mObj.Get();
+                        _uOldCpu = (ulong)mObj["UserModeTime"] + (ulong)mObj["KernelModeTime"];
+                    }
+                    else
+                    {
+                        _secondSample = DateTime.Now;
+                        mObj.Get();
+                        ulong u_newCPU = (ulong)mObj["UserModeTime"] + (ulong)mObj["KernelModeTime"];
+
+                        _msPassed = (_secondSample - _firstSample).TotalMilliseconds;
+                        var pu = (u_newCPU - _uOldCpu) / (_msPassed * 100.0 * Environment.ProcessorCount);
+
+                        _uOldCpu = u_newCPU;
+                        _firstSample = _secondSample;
+                        label1.Text = $"{pu:0}";
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Trace.Write(ex.ToString());
+                }
+            }
         }
 
         private void label1_DoubleClick(object sender, EventArgs e)
